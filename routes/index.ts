5f979293da88c5d6a804f6705435742a9d039581
XTsuola <2049434978@qq.com>
2026-04-10 14:03:54 +0800

    大清洗
    大清洗

--- routes/index.ts
+++ routes/index.ts
@@ -2,11 +2,6 @@ import { Router } from "https://deno.land/x/oak@v10.2.1/mod.ts";
 import { login } from "./login.ts";
 import { team } from "./team.ts";
 import { kaoshi } from "./kaoshi.ts";
-import { wzry } from "./wzry.ts";
-import { mhmnz } from "./mhmnz.ts";
-import { yuanshen } from "./yuanshen.ts";
-import { yys } from "./yys.ts";
-import { hywz } from "./hywz.ts";
 import { xingta } from "./xingta.ts";
 import { myLove } from "./myLove.ts";
 import { mota } from "./mota.ts";
@@ -17,11 +12,6 @@ const list = [
     login,
     team,
     kaoshi,
-    wzry,
-    mhmnz,
-    yuanshen,
-    yys,
-    hywz,
     xingta,
     mota,
     chess,
